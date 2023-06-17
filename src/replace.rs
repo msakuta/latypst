01b502d37f47f6b16f1e06c70bb01ee661221696
msakuta <masahiro.sakuta@gmail.com>
2023-06-17 17:46:54 +0900

    Add ddot -> dot.double replacement

--- src/replace.rs
+++ src/replace.rs
@@ -21,6 +21,7 @@ pub fn replace_cmd(elems: &[Element]) -> String {
             (Regex::new("varepsilon").unwrap(), "epsilon".to_string()),
             (Regex::new("int").unwrap(), "integral".to_string()),
             (Regex::new("vec").unwrap(), "arrow".to_string()),
+            (Regex::new("ddot").unwrap(), "dot.double".to_string()),
         ]
     });
 
